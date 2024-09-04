7a657a4b1aee31f4f6b48cc1d004484e02d3b433
Phattadol358 <phattadol@outlook.com>
2024-09-04 11:57:23 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="firstName" v-model="firstName" />
+      <input type="text" id="secondName" v-model="secondName" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputPage',
+  data() {
+    return {
+      title: 'วิวัฒนาการของโลกสมัยใหม่.',
+      nickname: 'พร้อม',
+      firstName: 'พัทธดนย์',
+      secondName: 'แก้วกัลยา',
+      studentCode : '358',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
